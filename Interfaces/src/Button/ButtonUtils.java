b943871600014c54f3bddb7eef0a17962d2268b5
YasmineZaatour <117782662+YasmineZaatour@users.noreply.github.com>
2023-12-19 22:54:56 +0100

    Initial Commit

---
+++ Interfaces/src/Button/ButtonUtils.java
@@ -0,0 +1,16 @@
+package Button;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ButtonUtils {
+    public static JButton createStyledButton(String text, int size) {
+        JButton button = new JButton(text);
+        button.setAlignmentX(Component.CENTER_ALIGNMENT);
+        button.setBackground(Color.WHITE);
+        button.setForeground(Color.BLACK);
+        button.setFont(new Font("Arial", Font.BOLD, size));
+        button.setPreferredSize(new Dimension(100, 50));
+        return button;
+    }
+}
